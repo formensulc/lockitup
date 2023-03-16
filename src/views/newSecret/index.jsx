c3e3784716fd8bbdee3872a2396a9fb4b2051485
Slavik <formensulc334@gmail.com>
2023-03-16 22:12:17 +0600

    Создана не полностью вторая страница

--- src/views/newSecret/index.jsx
+++ src/views/newSecret/index.jsx
@@ -1,9 +1,27 @@
-// import "./index.css"
+import "./index.css"
 function NewSecret() {
     return (
-       <div className="container">
-   hello react
-       </div>
+        <div className="homepage">
+          <div className="word">
+             <p className="oneTitle">Lock it up</p>
+          </div>
+              <hr />
+              <p className="question">how it works?  F.A.Q</p>
+              <p className="createSecret">Create a new Secret</p>
+              <textarea className="writeText" cols="30" rows="10"></textarea>  
+                <div className="numberDay">
+                  <p className="longSecret">How long needs to keep your secret?</p>
+                  <select className="select">
+                    <option value="1">1 day</option>
+                    <option value="2">2 day</option>
+                  </select>
+                </div>
+              <div className="unlockSecret">
+                <p>Phrase to unlock your secret</p>
+                <input className="input" type="text" placeholder="Enter the Pharse" />
+              </div>
+              <button className="oneBtn"><span>Lock message</span></button>           
+        </div>  
     )
 }
 
