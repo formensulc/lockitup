f6892023b1dfc139b74dcc6b214c5bed354e9fe0
Slavik <formensulc334@gmail.com>
2023-03-25 14:06:06 +0600

    был добавлен usestate и useform

--- src/views/newSecret/index.jsx
+++ src/views/newSecret/index.jsx
@@ -1,53 +1,76 @@
-import { Link } from "react-router-dom";
-import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useForm, Controller } from 'react-hook-form'
 import "./index.css"
 function NewSecret() {
-  const [text, setText] = useState("");
+
+  const [textAreaData, setTextAreaData] = useState("");
   const [days, setDays] = useState("1");
-  const [secret, setSecret] = useState("");
+  const [secretInput, setSecretInput] = useState("")
+  const navigate = useNavigate()
 
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue
+  } = useForm()
 
-  const handleDaysChange = (event) => {
-    setDays(event.target.value);
-  };
+  const onSubmit = (data) => {
+    console.log(data, "data");
+    navigate('/lock')
+  }
 
-  const handleSecretChange = (event) => {
-    setSecret(event.target.value);
-  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Data: ', textAreaData, days, secretInput);
+    navigate('/lock')
+  }
+  return (
+    <div className="newPage">
+      <form className="newBlock" onSubmit={handleSubmit(onSubmit)}>
+        <p className="newTitle">Lock it up</p>
+        <hr />
+        <p className="newOneText">how it works?  F.A.Q</p>
+        <p className="newTwoText">Create a new Secret</p>
+        <textarea value={textAreaData} className="newWriteText" placeholder="Start type your secret..." onChange={(e) => setTextAreaData(e.target.value)}></textarea>
+        <div className="newSelectBlock">
+          <p className="newThreeText">How long needs to keep your secret?</p>
+          <select className="newSeclect" onChange={(e) => setDays(e.target.value)}>
+            <option value="1">1 day</option>
+            <option value="2">2 day</option>
+          </select>
+        </div>
 
-  const handleLockMessage = () => {
-    localStorage.setItem("text", text);
-    localStorage.setItem("days", days);
-    localStorage.setItem("secret", secret);
-  };
-    return (
-        <div className="newPage">
-          <div className="newBlock">
-             <p className="newTitle">Lock it up</p>
-              <hr />
-               <p className="newOneText">how it works?  F.A.Q</p>
-               <p className="newTwoText">Create a new Secret</p>
-               <textarea className="newWriteText" placeholder="Start type your secret..." onChange={handleTextChange}></textarea> 
-
-              <div className="newSelectBlock">
-                <p className="newThreeText">How long needs to keep your secret?</p>
-                <select className="newSeclect" onChange={handleDaysChange}>
-                  <option value="1">1 day</option>
-                  <option value="2">2 day</option>
-                </select>
+        <div className="newUnlockBlock">
+          <p className="newFourTitle">Phrase to unlock your secret</p>
+          <Controller
+            control={control}
+            name="secretInput"
+            render={({ field: { onChange } }) => (
+              <div>
+                <input
+                  className="newInput"
+                  id="secretInput"
+                  placeholder="Enter the Pharse"
+                  onChange={onChange}
+                />
+                {errors?.secretInput && <span className="text-center"> <br /> Обязательное поле</span>}
               </div>
+            )}
+            {...register('secretInput', { required: true })}
+            ref={null}
+          />
+        </div>
+        <button
+          onClick={handleSubmit(onSubmit)}
+          className="newBtn">
+          Lock message</button>
+      </form>
 
-            <div className="newUnlockBlock">
-              <p className="newFourTitle">Phrase to unlock your secret</p>
-              <input className="newInput" type="text" placeholder="Enter the Pharse" onChange={handleSecretChange} />
-            </div>
-               <Link to={'/lock'}> <button className="newBtn" onClick={handleLockMessage}> <span>Lock message</span></button> </Link>   
-            </div>       
-         </div>  
-    )
+    </div>
+  )
 }
 
 export default NewSecret;
